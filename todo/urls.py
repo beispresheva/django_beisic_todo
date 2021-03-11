1b740d3a74838fe156984630eb19bc3bcf7a328f
Beis Presheva <beis.presheva@riinvest.net>
2021-03-12 00:11:11 +0100

    5 Main Pages HTML Structures finished

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="todo-index"),
+    path('today/', views.today, name="todo-today"),
+    path('upcoming/', views.upcoming, name="todo-upcoming"),
+    path('priority/', views.priority, name="todo-priority"),
+    path('label/', views.label, name="todo-label"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'todo/index.html')
+
+def today(request):
+    return render(request, 'todo/today.html', {'title': 'Today Tasks'})
+
+def upcoming(request):
+    return render(request, 'todo/upcoming.html', {'title': 'Upcoming Tasks'})
+
+def priority(request):
+    return render(request, 'todo/priority.html', {'title': 'Priorities'})
+
+def label(request):
+    return render(request, 'todo/label.html', {'title': 'Labels'})
