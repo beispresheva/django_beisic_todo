a6e367a5d85cbe6bd6187ea9b9209c429b7a338f
Beis Presheva <beis.presheva@riinvest.net>
2021-03-16 11:46:53 +0100

    DB structured and migrations

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Label Model
+class Label(models.Model):
+	label = models.CharField(max_length=30)
+
+	def __str__(self):
+		return self.label
+
+# Priority Model
+class Priority(models.Model):
+	priority = models.CharField(max_length=8)
+	
+	def __str__(self):
+		return self.priority
+
+# Task Model
+class Task(models.Model):
+	title = models.CharField(max_length=150)
+	description = models.TextField()
+	created_at = models.DateTimeField(default=timezone.now)
+	taskdate = models.DateTimeField()
+	completed = models.BooleanField(default=False)
+	priority = models.ForeignKey(Priority, on_delete=models.CASCADE)
+	label = models.ForeignKey(Label, on_delete=models.CASCADE)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.title
